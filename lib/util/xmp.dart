8e5f89a5b1bf3dfd671ed57eac1d0d7ea4099a22
Trevor Miller <tcmiller.dev@proton.me>
2026-08-01 14:06:37 +1000

    Create XMP files when copying RAW photos.

---
+++ lib/util/xmp.dart
@@ -0,0 +1,18 @@
+import '../model/raw_photo.dart';
+
+String getXMPForRawFile(RawPhoto rawPhoto) {
+  String rating = rawPhoto.rating==null ? '' : '<xmp:Rating>${rawPhoto.rating}</xmp:Rating>';
+  String color = rawPhoto.color==null ? '' : '<xmp:Label>${rawPhoto.color!.name}</xmp:Label>';
+  String selected = rawPhoto.selected ? '<xmpDM:good>True</xmpDM:good>' : '';
+
+  String xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n'
+      '<x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="XMP Core 6.0.0">\n'
+      '  <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">\n'
+      '    <rdf:Description rdf:about="" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/">\n'
+      '      $rating\n      $color\n      $selected\n'
+      '    </rdf:Description>\n'
+      '  </rdf:RDF>\n'
+      '</x:xmpmeta>';
+
+  return xml;
+}
